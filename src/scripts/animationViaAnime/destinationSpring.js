edf5931510d896e887a26fdeec44ce67b8a11030
Fahim Faisal <fahimfaisal.soikot@gmail.com>
2021-08-09 15:06:46 +0600

    feat: add random color of spring

--- src/scripts/animationViaAnime/destinationSpring.js
+++ src/scripts/animationViaAnime/destinationSpring.js
@@ -1,17 +1,25 @@
 import util from '../utility';
 
-const { anime } = util;
+const { anime, $ } = util;
 
 const { setDashoffset, random } = anime;
 
+const colors = ['#212832', '#df6951', '#f1a501', '#5e6282'];
+const target = $('.destination_spring > svg path');
+
 const springAnimation = () => {
+    const selectedColor = colors[random(0, colors.length - 1)];
+
     const animationObj = {
-        targets: '.destination_spring > svg path',
+        targets: target,
         strokeDashoffset: [setDashoffset, 0],
         easing: 'easeInQuad',
         duration: random(1000, 2500),
         direction: 'alternate',
-        complete: springAnimation
+        complete: () => {
+            target.style.stroke = selectedColor;
+            springAnimation();
+        }
     }
 
     anime(animationObj);
