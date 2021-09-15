c9caa23af570db8da83e30865d72650762e485fc
Fahim Faisal <fahimfaisal.soikot@gmail.com>
2021-09-15 23:17:02 +0600

    feat: add slider scripts

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './scripts/animationViaAnime/category';
 import './scripts/animationViaAnime/destinationSpring';
 import './scripts/animationViaAnime/hamburger';
 import './scripts/animationViaAnime/hero';
+import './scripts/animationViaAnime/testimonial';
 import "./styles.css";
 
---
+++ src/scripts/animationViaAnime/testimonial.js
@@ -0,0 +1,74 @@
+import utils from '../utility';
+
+const { $$, $ } = utils;
+
+const cardAnimationObj = {
+    targets: '.top-card',
+    top: ['10rem', '0rem'],
+    left: window.innerWidth > 770 ? ['30%', '25%'] : 0,
+    easing: 'easeInSine',
+    duration: 1000
+}
+
+// select testimonial card controllers svg and path
+const svg = $$('.card-controller > svg');
+const previousArrowIcon = $('#previous');
+const nextArrowIcon = $('#next');
+const testimonials = $$('.testimonial__right--card > *');
+const testimonialIndicators = $$('.testimonial__left-slideIndicator > *');
+
+let i = 0;
+let previousCard = null;
+let presentCard = testimonials[i];
+let nextCard = testimonials[i + 1];
+
+const indicatorController = index => {
+    testimonialIndicators.forEach(indicator => {
+        indicator.classList.remove('active-testimonial');
+    })
+
+    testimonialIndicators[index].classList.add('active-testimonial');
+}
+
+const cardController = e => {
+    const idName = e.target.id;
+    
+    if (idName === 'next' && nextCard) {
+        previousCard = presentCard;
+        presentCard = nextCard;
+    
+        nextCard = testimonials[i < testimonials.length ? ++i + 1 : i];
+
+        // set active arrow class to the previous icon
+        previousArrowIcon.firstElementChild.classList.add('active-arrow');
+
+        // switch the active dot
+        indicatorController(i);
+    }
+
+    if (idName === 'previous' && previousCard) {
+        nextCard = presentCard;
+        presentCard = previousCard;
+
+        i && i--;
+        previousCard = testimonials[i === 0 ? -1 : i + 1];
+
+        // set active arrow class to the next icon
+        nextArrowIcon.firstElementChild.classList.add('active-arrow');
+
+        // switch the active dot
+        indicatorController(i);
+    }
+
+    if (!nextCard) {
+        nextArrowIcon.firstElementChild.classList.remove('active-arrow');
+    }
+
+    if (!previousCard) {
+        previousArrowIcon.firstElementChild.classList.remove('active-arrow');
+    }
+}
+
+svg.forEach(arrow => {
+    arrow.addEventListener('click', cardController);
+})
